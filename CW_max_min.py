3a6b5936889a627d0b507edcd9e1481648fcb9f0
Sophiya-ai <sophiyabr@gmail.com>
2025-02-03 16:05:40 +0200

    CW and HW

---
+++ CW_max_min.py
@@ -0,0 +1,19 @@
+def find_max(list):
+    # создаём переменную для хранения максимального числа в массиве
+    max_number = list[0]
+    for i in list:
+        if i > max_number:
+            max_number = i
+    return max_number
+
+def find_min(list):
+    # создаём переменную для хранения максимального числа в массиве
+    min_number = list[0]
+    for i in list:
+        if i < min_number:
+            min_number = i
+    return min_number
+
+numbers = [56, 74, 23, 98, 143, -89, -23]
+print(find_max(numbers))
+print(find_min(numbers))
